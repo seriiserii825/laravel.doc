299bf6498bd2c6fa107c08298abf71d15fc13e9e
serii <seriiburduja@gmail.com>
2021-11-21 16:12:49 +0200

    seed

--- lv-seed.php
+++ lv-seed.php
@@ -9,5 +9,14 @@
 //seeders/DatabaseSeeders
  Director::factory(30)->create();
 
+// film Factory
+        return [
+            'name' => $this->faker->name,
+            'director_id' => $this->faker->unique()->numberBetween(1, 30)
+        ];
+//seeders/DatabaseSeeders
+Film::factory(30)->create();
+
+
 //console
 php artisan db:seed
