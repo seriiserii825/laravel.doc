61a37faef86186aa28fe39a43ab56513a53c1866
serii <seriiburduja@gmail.com>
2021-11-23 14:02:37 +0200

    some

--- lv-validate.php
+++ lv-validate.php
@@ -1,3 +1,28 @@
+// command
+php artisan make:request DeskStoreRequest
+
+Controller was created in Http/Requests
+
+//DeskStoreRequest
+<?php 
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:255'
+        ];
+    }
+ ?>
+
+//use this request to store or update
+<?php 
+    public function store(DeskStoreRequest $request)
+    {
+//use validated just for validated fields from DeskStoreRequest
+        $created_desk = Desk::create($request->validated());
+        return new DeskResource($created_desk);
+    }
+ ?>
+
 <?php 
 $request->validate([
     'title' => 'required',
