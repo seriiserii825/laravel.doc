e778ef7ced48dbc80ee4b7992e3b4205c1abb1eb
serii <seriiburduja@gmail.com>
2022-02-12 09:22:59 +0200

    feat: request validate

--- lv-validate.php
+++ lv-validate.php
@@ -3,31 +3,54 @@ php artisan make:request DeskStoreRequest
 
 Controller was created in Http/Requests
 
-//DeskStoreRequest
+Store
 <?php 
     public function rules()
     {
         return [
-            'name' => 'required|max:255'
+            'employee_id' =>  [
+                'required',
+                Rule::unique('salaries')
+                    ->where('employee_id', $this->employee_id)
+                    ->where('month', $this->month)
+            ],
+            'amount' => 'required',
+            'date' => 'required',
+            'month' => 'required',
+            'year' => 'required',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'employee_id.unique' => 'Combination of Employee id & month is not unique',
         ];
     }
  ?>
-
-//use this request to store or update
-<?php 
-    public function store(DeskStoreRequest $request)
+Update
+ <?php 
+    public function rules()
     {
-//use validated just for validated fields from DeskStoreRequest
-        $created_desk = Desk::create($request->validated());
-        return new DeskResource($created_desk);
+        return [
+            'employee_id' =>  [
+                'required',
+                Rule::unique('salaries')
+                    ->ignore($this->salary)
+                    ->where('employee_id', $this->employee_id)
+                    ->where('month', $this->month)
+            ],
+            'amount' => 'required',
+            'date' => 'nullable',
+            'month' => 'nullable',
+            'year' => 'nullable',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'employee_id.unique' => 'Combination of Employee id & month is not unique',
+        ];
     }
  ?>
-
-<?php 
-$request->validate([
-    'title' => 'required',
-    'description' => 'required',
-    'content' => 'required',
-    'category_id' => 'required|integer',
-    'thumbnail' => 'nullable|image',
-]);
