a2036188fbcc54b0a2f0e038e53808f2416dda23
serii <seriiburduja@gmail.com>
2022-02-15 21:40:33 +0200

    feat: request

---
+++ store-request.php
@@ -0,0 +1,34 @@
+<?php
+
+class StoreSalaryRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'employee_id' =>  [
+                'required',
+                Rule::unique('salaries')
+                    ->where('employee_id', $this->employee_id)
+                    ->where('month', $this->month)
+            ],
+            'amount' => 'required',
+            'date' => 'required',
+            'month' => 'required',
+            'year' => 'required',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'employee_id.unique' => 'Combination of Employee id & month is not unique',
+        ];
+    }
+}
+
+?>
---
+++ update-request.php
@@ -0,0 +1,32 @@
+<?php 
+class UpdateSalaryRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'employee_id' =>  [
+                'required',
+                Rule::unique('salaries')
+                    ->ignore($this->salary)
+                    ->where('employee_id', $this->employee_id)
+                    ->where('month', $this->month)
+            ],
+            'amount' => 'required',
+            'date' => 'nullable',
+            'month' => 'nullable',
+            'year' => 'nullable',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'employee_id.unique' => 'Combination of Employee id & month is not unique',
+        ];
+    }
+}
