5f942163fe967e193c5fbd32d353385852c3fbff
serii <seriiburduja@gmail.com>
2022-07-30 08:06:30 +0300

    refactor: update-request

--- update-request.php
+++ update-request.php
@@ -1,46 +1,43 @@
-<?php 
+<?php
 class UpdateSalaryRequest extends FormRequest
 {
-    public function authorize()
-    {
-        return true;
-    }
+  public function authorize()
+  {
+    return true;
+  }
 
-    public function rules()
-    {
-        return [
-            'employee_id' =>  [
-                'required',
-                Rule::unique('salaries')
-                    ->ignore($this->salary)
-                    ->where('employee_id', $this->employee_id)
-                    ->where('month', $this->month)
-            ],
-            'amount' => 'required',
-            'date' => 'nullable',
-            'month' => 'nullable',
-            'year' => 'nullable',
-        ];
-    }
-
-    public function messages()
-    {
-        return [
-            'employee_id.unique' => 'Combination of Employee id & month is not unique',
-        ];
-    }
-}
-
-public function update(Request $request, Post $post)
-{
-    $validatedData = $request->validate([
-        'name' => ['required', 'max:60', Rule::unique('posts')->ignore($post->post)],
-    ]);
-}
-
-public function rules()
-{
+  public function rules()
+  {
     return [
-        'email' => "required|unique:users,email,{$this->user}"
-    ]; 
+      'employee_id' =>  [
+        'required',
+        Rule::unique('salaries')
+          ->ignore($this->salary)
+          ->where('employee_id', $this->employee_id)
+          ->where('month', $this->month)
+      ],
+      'amount' => 'required',
+      'date' => 'nullable',
+      'month' => 'nullable',
+      'year' => 'nullable',
+    ];
+  }
+
+  public function messages()
+  {
+    return [
+      'employee_id.unique' => 'Combination of Employee id & month is not unique',
+    ];
+  }
+
+  public function rules()
+  {
+    return [
+      'name' => [
+        'required',
+        Rule::unique('categories')->ignore($this->category)
+      ],
+      'status' => 'required|integer',
+    ];
+  }
 }
